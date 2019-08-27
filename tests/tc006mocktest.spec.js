b743308e1ee75650d8a1c5a6e5522ed43a35f21f
Smita Dutta <dsmita22@gmail.com>
2019-08-27 23:31:14 +1000

    added mock test

---
+++ tests/tc006mocktest.spec.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const should = require('should');
+const supertest = require('supertest');
+const api = supertest('http://localhost:3000' || process.env.TEST_ENV);
+
+describe('Test mock api', () => {
+
+    it('Test random', async () => {
+        const res = await api.get('/employees')
+            .expect(200);
+        console.log(res.body);
+        
+    })
+
+})
+
